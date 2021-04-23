e0f5a121b1bca4918b73cf5b933bbedc4ca0852e
Rodrigo Miguel <maiguel.rodrigo@gmail.com>
2021-04-23 10:13:01 -0300

    soluciono errores de versiones

--- fns.js
+++ fns.js
@@ -22,15 +22,11 @@ function mul(a, b) {
 
 }
 function div(a, b) {
-<<<<<<< HEAD
-=======
-
->>>>>>> 549611a54e39d09576c41d6d05a98a97ebb505a5
 
     // Tener en cuenta division por 0
     // Si b es 0 retornar "Error: div by 0" 
     
-  if ( b == 0) {
+  if ( b === 0) {
         
     console.log('Error: div by 0 !');	
 
